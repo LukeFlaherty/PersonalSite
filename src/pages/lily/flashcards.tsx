86b31633ec39eccca4f2b879974f0ff3845433a0
Luke <flaherty.luke@yahoo.com>
2023-12-06 01:21:25 -0500

    flex styling

--- src/pages/lily/flashcards.tsx
+++ src/pages/lily/flashcards.tsx
@@ -110,130 +110,154 @@ const FlashcardsPage: React.FC = () => {
 	}
 
 	return (
-		<Container as="main" mt={5}>
+		<Container as="main" mt={5} sx={{ maxWidth: '100%' }}>
 			<Head>
 				<title>Flashcards Page</title>
 			</Head>
 
-			<Heading>Create Flashcards</Heading>
-
-			{/* Form to create a new flashcard set */}
-			<Flex sx={{ flexDirection: 'column' }}>
-				<Input
-					type="text"
-					placeholder="Flashcard Set Title"
-					value={currentSet.title}
-					onChange={(e) =>
-						setCurrentSet({ ...currentSet, title: e.target.value })
-					}
-				/>
-				{currentSet.cards.map((card, index) => (
-					<Flex key={index}>
-						<Input
-							type="text"
-							placeholder="Front of Card"
-							value={card.front}
-							onChange={(e) => {
-								const newCards = [...currentSet.cards]
-								newCards[index].front = e.target.value
-								setCurrentSet({ ...currentSet, cards: newCards })
-							}}
-						/>
-						<Input
-							type="text"
-							placeholder="Back of Card"
-							value={card.back}
-							onChange={(e) => {
-								const newCards = [...currentSet.cards]
-								newCards[index].back = e.target.value
-								setCurrentSet({ ...currentSet, cards: newCards })
-							}}
-						/>
-					</Flex>
-				))}
-				{/* Dropdown to select an existing set */}
-				<select
-					value={selectedSetIndex}
-					onChange={(e) => handleSelectSet(Number(e.target.value))}
-				>
-					<option value={-1}>Select a Set</option>
-					{flashcardSets.map((set, index) => (
-						<option key={index} value={index}>
-							{set.title}
-						</option>
+			{/* Main Flex Container for the two sections */}
+			<Flex sx={{ flexDirection: ['column', 'row'], width: '100%' }}>
+				{/* Left Section: List of Sets */}
+				<Box sx={{ width: ['100%', '50%'], pr: [0, 3], mb: [4, 0] }}>
+					<Heading as="h3" mb={3}>
+						Flashcard Sets
+					</Heading>
+					{flashcardSets.map((set, setIndex) => (
+						<Box
+							key={setIndex}
+							mb={3}
+							sx={{ bg: 'muted', p: 3, borderRadius: '4px' }}
+						>
+							<Text sx={{ fontSize: 3, fontWeight: 'bold' }}>{set.title}</Text>
+							<Button mr={2} onClick={() => startEditingSet(setIndex)}>
+								Edit Set
+							</Button>
+							<Button onClick={() => deleteSet(setIndex)}>Delete Set</Button>
+							{set.cards.map((card, cardIndex) => (
+								<Flex key={cardIndex} mt={2} sx={{ alignItems: 'center' }}>
+									<Text sx={{ fontSize: 2, mr: 2 }}>
+										{card.front} - {card.back}
+									</Text>
+									<Button
+										mr={2}
+										onClick={() => startEditingCard(setIndex, cardIndex)}
+									>
+										Edit Card
+									</Button>
+									<Button onClick={() => deleteCard(setIndex, cardIndex)}>
+										Delete Card
+									</Button>
+								</Flex>
+							))}
+						</Box>
 					))}
-				</select>
-				<Button onClick={addCardToSet}>Add Card to Set</Button>
-				<Button onClick={addSet}>Create Set</Button>
-			</Flex>
+				</Box>
 
-			{/* List of existing flashcard sets */}
-			<Box>
-				<Heading>Flashcard Sets</Heading>
-				{/* List of existing flashcard sets with edit and delete options */}
-				{flashcardSets.map((set, setIndex) => (
-					<div key={setIndex}>
-						<Text>{set.title}</Text>
-						<Button onClick={() => startEditingSet(setIndex)}>Edit Set</Button>
-						<Button onClick={() => deleteSet(setIndex)}>Delete Set</Button>
-						{set.cards.map((card, cardIndex) => (
-							<div key={cardIndex}>
-								<Text>
-									{card.front} - {card.back}
-								</Text>
-								<Button onClick={() => startEditingCard(setIndex, cardIndex)}>
-									Edit Card
-								</Button>
-								<Button onClick={() => deleteCard(setIndex, cardIndex)}>
-									Delete Card
-								</Button>
-							</div>
+				{/* Right Section: Flashcard Creation and Editing */}
+				<Box sx={{ width: ['100%', '50%'], pl: [0, 3] }}>
+					<Heading as="h3" mb={3}>
+						Create/Edit Flashcards
+					</Heading>
+					<Flex sx={{ flexDirection: 'column', mb: 4 }}>
+						<Input
+							mb={3}
+							type="text"
+							placeholder="Flashcard Set Title"
+							value={currentSet.title}
+							onChange={(e) =>
+								setCurrentSet({ ...currentSet, title: e.target.value })
+							}
+						/>
+						{currentSet.cards.map((card, index) => (
+							<Flex key={index} mb={2}>
+								<Input
+									mr={2}
+									type="text"
+									placeholder="Front of Card"
+									value={card.front}
+									onChange={(e) => {
+										const newCards = [...currentSet.cards]
+										newCards[index].front = e.target.value
+										setCurrentSet({ ...currentSet, cards: newCards })
+									}}
+								/>
+								<Input
+									type="text"
+									placeholder="Back of Card"
+									value={card.back}
+									onChange={(e) => {
+										const newCards = [...currentSet.cards]
+										newCards[index].back = e.target.value
+										setCurrentSet({ ...currentSet, cards: newCards })
+									}}
+								/>
+							</Flex>
 						))}
-					</div>
-				))}
-				{/* Editing Interface for Set Title */}
-				{isEditingSet && editingSetIndex >= 0 && (
-					<div>
-						<Input
-							type="text"
-							value={tempSet.title}
-							onChange={(e) =>
-								setTempSet({ ...tempSet, title: e.target.value })
-							}
-						/>
-						<Button onClick={saveEdits}>Save Set Title</Button>
-					</div>
-				)}
+						<Box mb={3}>
+							<select
+								value={selectedSetIndex}
+								onChange={(e) => handleSelectSet(Number(e.target.value))}
+							>
+								<option value={-1}>Select a Set</option>
+								{flashcardSets.map((set, index) => (
+									<option key={index} value={index}>
+										{set.title}
+									</option>
+								))}
+							</select>
+						</Box>
+						<Button mr={2} onClick={addCardToSet}>
+							Add Card to Set
+						</Button>
+						<Button onClick={addSet}>Create Set</Button>
+					</Flex>
 
-				{/* Editing Interface for Card Details */}
-				{!isEditingSet && editingCardIndex >= 0 && (
-					<div>
-						<Input
-							type="text"
-							placeholder="Front of Card"
-							value={tempSet.cards[0].front}
-							onChange={(e) =>
-								setTempSet({
-									...tempSet,
-									cards: [{ ...tempSet.cards[0], front: e.target.value }],
-								})
-							}
-						/>
-						<Input
-							type="text"
-							placeholder="Back of Card"
-							value={tempSet.cards[0].back}
-							onChange={(e) =>
-								setTempSet({
-									...tempSet,
-									cards: [{ ...tempSet.cards[0], back: e.target.value }],
-								})
-							}
-						/>
-						<Button onClick={saveEdits}>Save Card</Button>
-					</div>
-				)}
-			</Box>
+					{/* Editing Interface for Set Title */}
+					{isEditingSet && editingSetIndex >= 0 && (
+						<div>
+							<Input
+								type="text"
+								value={tempSet.title}
+								onChange={(e) =>
+									setTempSet({ ...tempSet, title: e.target.value })
+								}
+							/>
+							<Button onClick={saveEdits}>Save Set Title</Button>
+						</div>
+					)}
+
+					{/* Editing Interface for Card Details */}
+					{!isEditingSet && editingCardIndex >= 0 && (
+						<div>
+							<Input
+								mb={2}
+								type="text"
+								placeholder="Front of Card"
+								value={tempSet.cards[0].front}
+								onChange={(e) =>
+									setTempSet({
+										...tempSet,
+										cards: [{ ...tempSet.cards[0], front: e.target.value }],
+									})
+								}
+							/>
+							<Input
+								mb={2}
+								type="text"
+								placeholder="Back of Card"
+								value={tempSet.cards[0].back}
+								onChange={(e) =>
+									setTempSet({
+										...tempSet,
+										cards: [{ ...tempSet.cards[0], back: e.target.value }],
+									})
+								}
+							/>
+							<Button onClick={() => saveEdits()}>Save Card</Button>
+						</div>
+					)}
+				</Box>
+			</Flex>
 		</Container>
 	)
 }
